69e6ddcc77d8b02a8d0f64130771db393cb18109
Visteo <arumdaovisteo@gmail.com>
2024-11-17 22:12:40 +0700

    Baru

--- application/views/messages.php
+++ application/views/messages.php
@@ -1,5 +1,5 @@
 <?php if($this->session->has_userdata('success')) { ?> 
-    <div class="alert alert-succes alert-dismissible">
+    <div class="alert alert-success alert-dismissible">
     <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
     <i class="icon fa fa-check"></i> <?=$this->session->flashdata('success')?>
 </div>
